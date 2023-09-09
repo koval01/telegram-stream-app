08bb14104a6f2e5eb8470315b8843c3ce0c33d78
Koval Yaroslav <yaroslav@koval.page>
2023-09-09 22:32:41 +0300

    update structure

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,16 +1,17 @@
-# This file is part of https://github.com/jainamoswal/Flask-Example.
-# Usage covered in <IDC lICENSE>
-# Jainam Oswal. <jainam.me> 
-
-
-# Import Libraries 
 import os
 import importlib
 
-# Get all files.
-views = [f for f in os.listdir(os.path.dirname(os.path.abspath(__file__))) if f.endswith(".py") and f != "__init__.py"]
 
-# Import all files from modules folder.
+views = [
+    f for f in os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    if f.endswith(".py") and f != "__init__.py"
+]
+
+
 for view in views:
-    importlib.import_module(os.path.dirname(os.path.realpath(__file__)).split('/')[-1] + "." + view[:-3])
-    print('App imported ' + view + ' successfully.')
+    importlib.import_module(
+        os.path.dirname(
+            os.path.realpath(__file__)
+        ).split('/')[-1] + "." + view[:-3]
+    )
+    print(f'App imported {view} successfully.')
--- modules/errors.py
+++ modules/errors.py
@@ -1,37 +1,45 @@
-# This file is part of https://github.com/jainamoswal/Flask-Example.
-# Usage covered in <IDC lICENSE>
-# Jainam Oswal. <jainam.me> 
+from flask import render_template
 
-
-# Import Libraries 
 from app import app
+
 
 # Define Error handler for 400
 @app.errorhandler(400)
 def error_400(e):
-    return "400, bad request.", 400
+    return {"code": 400, "message": "bad request"}, 400
+
 
 # Define Error handler for 404
 @app.errorhandler(404)
 def error_404(e):
-    return "404, page not found.", 404
+    return render_template('nothing.html'), 404
+
 
 # Define Error handler for 403
 @app.errorhandler(403)
 def error_403(e):
-    return "403, you are not authorized to see resource.", 403
+    return {"code": 403, "message": "forbidden"}, 403
+
 
 # Define Error handler for 405
 @app.errorhandler(405)
 def error_405(e):
-    return "405, HTTP method not allowed.", 405
+    return {"code": 405, "message": "method not allowed"}, 405
+
 
 # Define Error handler for 408
 @app.errorhandler(408)
 def error_408(e):
-    return "408, your request is taking too long to be served.}", 408
+    return {"code": 408, "message": "your request is taking too long to be served"}, 408
+
 
 # Define Error handler for 418
 @app.errorhandler(418)
 def error_418(e):
-    return "Yes, I am a Teapot.", 418
+    return {"code": 418, "message": "Yes, I am a Teapot"}, 418
+
+
+# Define Error handler for 429
+@app.errorhandler(429)
+def error_429(e):
+    return {"code": 429, "message": "Too Many Requests"}, 429
